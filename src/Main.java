1c3f298b262991c1d4e9550a2923f2d9fef97f0e
Ahmed saleh <salehdah@hotmail.com>
2022-08-24 01:15:57 -0400

    encapsulation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 package src;
 
+import src.encapsulation.EncapsulationDemo;
+
 public class Main{
   // this is our first file in this collaboration work
+  public static void main(String[] args) {
+      EncapsulationDemo demo = new EncapsulationDemo(189, "dah");
+      System.out.println(demo.getStr());
+  }
 }
