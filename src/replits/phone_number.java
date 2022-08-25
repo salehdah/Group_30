e358576f79eec5e60cbe55a312e5f054b0b851fa
salehdah <107492486+salehdah@users.noreply.github.com>
2022-08-24 21:27:12 -0400

    Create phone_number.java

---
+++ src/replits/phone_number.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class Main {
+ public static void main(String[] args){
+    //DO NOT TOUCH THESE LINE/LINES.
+    // Variables are already declared and given
+    Scanner scan=new Scanner(System.in);
+    int areaCode=scan.nextInt();
+    int localNumber=scan.nextInt();
+    
+    //WRITE YOUR CODE BELOW:
+   System.out.println("Calling number ("+areaCode+")-"+localNumber);
+    
+  }
+}
