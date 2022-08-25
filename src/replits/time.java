a6c69c1463dbd4bef2609800b77d4540d2a5247b
salehdah <107492486+salehdah@users.noreply.github.com>
2022-08-24 21:29:14 -0400

    Create time.java

---
+++ src/replits/time.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    //1. WRITE YOUR CODE HERE:
+
+    
+    
+    //DO NOT TOUCH BELOW LINES.
+    Scanner sc = new Scanner(System.in);
+    int hour   = sc.nextInt();
+    int minute = sc.nextInt();
+    int second = sc.nextInt();
+    String amOrPm = sc.next();
+    //#############################
+    
+     //2. WRITE YOUR CODE HERE:
+     
+    System.out.println(hour+":"+minute+":"+second+" "+amOrPm);
+     
+
+  }
+}
