c53c56dbabd73dc766b7e4f86b261ca133e5f6cb
Ahmed saleh <salehdah@hotmail.com>
2022-08-24 21:07:41 -0400

    encapsulation

---
+++ src/replits/Hello_World1.java
@@ -0,0 +1,9 @@
+package src.replits;
+public class Hello_World1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+
+    }
+}
+
---
+++ src/replits/Hello_World2.java
@@ -0,0 +1,10 @@
+package src.replits;
+
+public class Hello_World2{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+
+
+}
