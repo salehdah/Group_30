a834bacbfbe9c2fbf089fa7fbfa0f41972220bac
Ahmed saleh <salehdah@hotmail.com>
2022-08-24 22:18:35 -0400

    renamed main to encapsulation

--- src/encapsulation/Encapsulation.java
+++ src/encapsulation/Encapsulation.java
@@ -1,8 +1,6 @@
 package src.encapsulation;
 
-import src.encapsulation.EncapsulationDemo;
-
-public class Main{
+public class Encapsulation {
   // this is our first file in this collaboration work
   public static void main(String[] args) {
       EncapsulationDemo demo = new EncapsulationDemo(185, "dah");
