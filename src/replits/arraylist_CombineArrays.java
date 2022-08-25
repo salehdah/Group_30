c5093b96ab15a527ce7fdb8484b47c439f21f8e6
salehdah <107492486+salehdah@users.noreply.github.com>
2022-08-24 21:52:53 -0400

    Create arraylist_CombineArrays.java

---
+++ src/replits/arraylist_CombineArrays.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+import java.util.*;
+class Main {
+  public static ArrayList<String> combine(String[] r1, String[] r2){
+
+    // your code here
+    ArrayList<String> list= new ArrayList<>();
+  for(int i=0; i<r1.length; i++ ){
+    list.add(r1[i]);
+    
+  }
+for(int i=0; i<r2.length; i++){
+  list.add(r2[i]);
+}
+return list;
+
+  
+  
+   
+  }
+
+  public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      String [] strs = new String[in.nextInt()];
+      String [] strs2 = new String[in.nextInt()];
+      for(int i=0; i < strs.length; i++) {
+        strs[i] = in.next();
+      }
+      for(int i=0; i < strs2.length; i++) {
+        strs2[i] = in.next();
+      }
+
+    System.out.println(combine(strs,strs2));
+
+  } 
+
+}
