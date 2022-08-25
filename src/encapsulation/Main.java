e15ec4df8e3645bd78b63c048be49a1ec087fdba
Ahmed saleh <salehdah@hotmail.com>
2022-08-24 22:15:37 -0400

    moved main to encapsulation

--- src/encapsulation/Main.java
+++ src/encapsulation/Main.java
@@ -5,7 +5,7 @@ import src.encapsulation.EncapsulationDemo;
 public class Main{
   // this is our first file in this collaboration work
   public static void main(String[] args) {
-      EncapsulationDemo demo = new EncapsulationDemo(189, "dah");
+      EncapsulationDemo demo = new EncapsulationDemo(185, "dah");
       System.out.println(demo.getStr());
   }
 }
